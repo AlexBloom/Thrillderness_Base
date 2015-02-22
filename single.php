13bd51a5369cbae383924f2e7d634778e19d9480
Alex Bloom <alexbloomdesign@gmail.com>
2015-02-21 19:29:46 -0500

    Dialing in Homepage Post Grid

--- single.php
+++ single.php
@@ -1,6 +1,68 @@
 <?php include_once'inc/head.inc' ?>
 <?php include_once'inc/header.inc' ?>
 
-        <p> Hello self! This is HTML5 Boilerplate with includes. </p>
+		<article>	
+			<header>
+				
+				
+				<div class="featured-image"> 
+						
+				</div>
+				
+				<p> <a href="talkin" class="category-link">Talkin'</a> </p>
+				<h1 class="title"> Snorkeling with Adam Fram </h1>
+				<p class="subtitle"> Don't be a dick underwater. </p>
+				<p> From <a class="author">Alex</a> on <span class="date"> 2015.03.01 </span> </p>
+				
+			</header>
+			
+			
+			<section class="content">
+				<p class="question"> Question from alex, how do you get into snorkeling, how long have you been doing it? </p>
+				<p> Answer from Adam, I've been doing this for about 6 months every day, but on and off for about 4 years prior. I've had a job working with the manta ray advocates in Hawaii that has given me a ton of experience.
+				</p>
+				<p> Tons of sample text with a soft return <br>
+				and some more text for this paragraph.
+				</p>
+				<blockquote>
+					Text from someone from somewhere. Font Size Idea: px at the Root, rem for Components, em for Text Elements
+				</blockquote>
+				<p> Tons of sample text with a soft return <br>
+				and some more text for this paragraph.
+				</p>
+			</section>
+			
+			<aside class="meta">
+				This is our sidebar text. there could be a ton which could effect the column width.
+			</aside>
+			
+			<footer>
+				
+				<nav class="share-nav"> 
+					<ul>
+						<li> <span class="icon-facebook"> </span> share </li>
+						<li> <span class="icon-twitter"> </span> tweet </li>
+						<li> <span class="icon-tumblr"> </span> email </li>
+					</ul>		
+				</nav>
+				
+				<nav class="tag-nav"> 
+					<ul>
+						<li> <a href=""> Hawaii </a> </li>
+						<li> <a href=""> Snorkeling </a> </li>
+						<li> <a href=""> Scuba Diving </a> </li>
+						<li> <a href=""> Oceans </a> </li>
+					</ul>
+				</nav>
+				
+				<div id="comments"> 
+					Disqus comment feed
+				</div>
+				
+			</footer>
+		</article>
+		<div class="pagination">
+			
+		</div>
 
 <?php include_once'inc/footer.inc' ?>
